6f818953e88cfc88d4a428fea7f4f39e374a90f2
lurongtao <lurongtao@126.com>
2016-10-16 11:49:13 +0800

    abc

--- pages/footballsns/media/media.js
+++ pages/footballsns/media/media.js
@@ -5,7 +5,9 @@ Page({
     poster: '/media/xuwei.webp',
     name: '此时此刻',
     author: '许巍',
-    src: '/media/now.mp3'
+    src: '/media/now.mp3',
+
+    imgUrl: ['http://placehold.it/36x36']
   },
   onLoad: function () {
     var that = this;
@@ -22,9 +24,75 @@ Page({
         });
         clearInterval(timer);
       }
-    }.bind(this), 30);
+    }.bind(this), 10);
   },
   onShow: function () {
 
+  },
+
+  // 注：开发工具貌似不能录音
+  startRecord: function () {
+    wx.startRecord({
+      success: function(res) {
+        var tempFilePath = res.tempFilePath
+        console.log(0);
+      },
+      fail: function(res) {
+         //录音失败
+      }
+    })
+  },
+  stopRecord: function () {
+    console.log('stop');
+  },
+
+  chooseImage: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          imgUrl: res.tempFilePaths
+        })
+      }
+    })
+  },
+  uploadFile: function () {
+    // 上传文件
+    wx.uploadFile({
+      url: 'http://felixlu.bceapp.com/upload.php',
+      filePath: this.data.imgUrl[0],
+      name: 'file',
+      formData:{
+        'user': 'test'
+      },
+      success: function (res) {
+        console.log("服务器返回信息：" + res.data);
+      }
+    })
+  },
+  downloadFile: function () {
+    // 下载文件
+    var that = this;
+    wx.downloadFile({
+      url: 'http://felixlu.bceapp.com/files/img4.jpg',
+      type: 'audio',
+      success: function(res) {
+        // 保存文件到本地
+        wx.saveFile({
+          tempFilePath: res.tempFilePath,
+          success: function(res) {
+            var savedFilePath = res.savedFilePath;
+            console.log('本地文件路径：' + savedFilePath);
+            that.setData({
+              'imgUrl[0]': res.savedFilePath
+            })
+          }
+        })
+      }
+    })
   }
 });
--- pages/footballsns/search/search.js
+++ pages/footballsns/search/search.js
@@ -1,8 +1,59 @@
 Page({
   data: {
+    latitude: 40.056601,
+    longitude: 116.334655,
+    speed: 0,
+    accuracy: 0,
 
+    networkType: ''
   },
   onLoad: function () {
-    // console.log(0);
+
+  },
+  onShow: function () {
+    var that = this;
+    wx.getNetworkType({
+      success: function(res) {
+        var networkType = res.networkType // 返回网络类型2g，3g，4g，wifi
+        that.setData({
+          networkType: networkType
+        })
+      }
+    });
+
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log('手机型号:' + res.model)
+        console.log('设备像素比:' + res.pixelRatio)
+        console.log('窗口宽度:' + res.windowWidth)
+        console.log('窗口高度:' + res.windowHeight)
+        console.log('微信设置的语言:' + res.language)
+        console.log('微信版本号:' + res.version)
+      }
+    });
+  },
+  showInWechatMap: function () {
+    var that = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success: function(res) {
+        // that.setData({
+        //   latitude: res.latitude,
+        //   longitude: res.longitude,
+        //   speed: res.speed,
+        //   accuracy: res.accuracy
+        // });
+
+        // 使用微信内置地图查看位置
+        wx.openLocation({
+          latitude: that.data.latitude,
+          longitude: that.data.longitude,
+          scale: 28
+        });
+      },
+      fail: function () {
+        console.log('开发工具不支持~');
+      }
+    })
   }
 });
